2878e47630f13722d9a83a5583e5ecee3bfc228c
SANKET PRAJAPATI <87607558+sanket913@users.noreply.github.com>
2025-07-25 20:16:03 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -30,7 +30,7 @@ const Contact: React.FC = () => {
     setSubmitStatus({ type: null, message: '' });
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://tech-exzakt.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
