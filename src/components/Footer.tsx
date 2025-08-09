80d143501850be6d2a6f6bed6fe6ad47add4ada4
SANKET PRAJAPATI <87607558+sanket913@users.noreply.github.com>
2025-08-09 11:09:06 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Mail, Phone, MapPin, ArrowRight, Facebook, Twitter, Instagram, Linkedin, Github } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   const services = [
